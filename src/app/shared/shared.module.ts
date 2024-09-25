ea754e2ce620b2397852619a9e3a8097cb78dbbb
elvisnjintze <enjintze@gmail.com>
2024-09-25 19:12:13 +0100

    Nous avons crée La Directive qui pourra être utilisée dans toute l'application, donc elle a tout intérêt à être dans SharedModule. Dans le dossier  shared  , créez un dossier  directives  et un fichier  highlight.directive.ts cette directive permet d'appliquer un background de couleur jaune sur la balise html dans laquelle elle est incorporée

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,12 +8,13 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { ShortenPipe } from './pipes/shoten.pipe';
 import { LastNameFirstName } from './pipes/lastnamefirtsname.pipe';
 import { TimeAgoPipe } from './pipes/time-ago.pipe';
+import { HighlightDirective } from './directives/highligh.directice';
 
 @NgModule({
   // nous avons conçu des pipes personalisés ShortenPipe
   //et LastNameFirstName que nous déclarons et exportons(dans)
   //pour etre utiliser dans toute l'application
-  declarations: [ShortenPipe,LastNameFirstName,TimeAgoPipe],
+  declarations: [ShortenPipe,LastNameFirstName,TimeAgoPipe,HighlightDirective],
   //nous avons enlevé MatCardModule des imports et des exports
   // pour remplacer avec le module MaterialModule
   // qui concentre toutes les modules Matérial que nous allons utilisé
@@ -22,7 +23,7 @@ import { TimeAgoPipe } from './pipes/time-ago.pipe';
     CommonModule, MaterialModule,CommentsComponent,ReactiveFormsModule
   ],
   //exports: [MatToolbarModule,MatCardModule,CommentsComponent
-    exports:[CommentsComponent,MaterialModule,ReactiveFormsModule,ShortenPipe, LastNameFirstName,TimeAgoPipe]
+    exports:[CommentsComponent,MaterialModule,ReactiveFormsModule,ShortenPipe, LastNameFirstName,TimeAgoPipe,HighlightDirective]
   
 })
 export class SharedModule { }
