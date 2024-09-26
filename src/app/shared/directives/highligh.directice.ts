b31453e77cfed2a55d66da43e15e6459e31f612f
elvisnjintze <enjintze@gmail.com>
2024-09-26 08:59:34 +0100

    Il peut être très intéressant, pour une Directive, de pouvoir réagir aux événements HTML émanant de l'élément sur lequel elle est placée. Angular vous permet de faire exactement ça avec le décorateur @HostListener  : Dans notre cas :
    quand la souris entre sur l'élément, vous changez sa couleur de fond en  lightgreen  ;
    
    si la souris sort sans avoir cliqué, la couleur revient à sa couleur par défaut ;
    
    si l'utilisateur clique sur l'élément, la couleu par défaut est changée en  lightgreen  , ce qui donne l'effet de valider ce qui est cliqué : l'élément restera  lightgreen  en permanence. Une Directive peut être placée sur différents types d'élément HTML pour leur apporter un comportement supplémentaire.
    
    Le selector d'une Directive attribut doit s'écrire entre crochets  []  .
    
    Une Directive peut injecter l'élément HTML sur lequel elle est placée avec  ElementRef  , et interagir avec cet élément avec  Renderer2  .
    
    Une Directive peut comporter des  @Input  pour accepter des paramètres.
    
    Une Directive peut écouter les événements émanant de son élément grâce au décorateur  @HostListener

--- src/app/shared/directives/highligh.directice.ts
+++ src/app/shared/directives/highligh.directice.ts
@@ -1,18 +1,29 @@
-import { AfterViewInit, Directive, ElementRef, Input, Renderer2 } from '@angular/core';
+import { AfterViewInit, Directive, ElementRef, HostListener, Input, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[highlight]'
 })
 export class HighlightDirective implements AfterViewInit {
-  @Input() highlight = 'yellow';
+  @Input() color = 'yellow';
   constructor(private el: ElementRef,
               private renderer: Renderer2) {}
 
   ngAfterViewInit() {
-    this.setBackgroundColor(this.highlight);
+    this.setBackgroundColor(this.color);
   }
 
   setBackgroundColor(color: string) {
     this.renderer.setStyle(this.el.nativeElement, 'background-color', color);
+  }
+  @HostListener('mouseenter') onMouseEnter() {
+    this.setBackgroundColor('lightgreen');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.setBackgroundColor(this.color);
+  }
+
+  @HostListener('click') onClick() {
+    this.color = 'lightgreen';
   }
 }
