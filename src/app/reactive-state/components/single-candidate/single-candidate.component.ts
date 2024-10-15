20cce0b305867943b70e8b2162fab8f08b89f314
elvisnjintze <enjintze@gmail.com>
2024-10-15 18:15:01 +0100

    Le state management réactif, une fois implémenté, facilite largement l'implémentation de nouvelles fonctionnalités, car les components affichent simplement ce que le service leur envoie.
    Vous pouvez calculer la différence entre  Date.now()  et un timestamp pour connaître le temps depuis une dernière requête.
    
    ng-container  permet d'attribuer des directives structurelles sans modifier la structure du document HTML.
    
    *ngIf  accepte une référence à un  ng-template  en  else  qui sera affiché si la condition n'est pas remplie.
    
    les  ng-template  ne sont pas insérés dans le DOM par défaut, et permettent de faire référence à des blocs de HTML.

--- src/app/reactive-state/components/single-candidate/single-candidate.component.ts
+++ src/app/reactive-state/components/single-candidate/single-candidate.component.ts
@@ -1,13 +1,43 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Observable, switchMap } from 'rxjs';
+import { Candidate } from '../../models/candidate.model';
+import { CandidateService } from '../../services/candidatas.service';
+import { SharedModule } from '../../../shared/shared.module';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-single-candidate',
   standalone: true,
-  imports: [],
+  imports: [SharedModule,CommonModule],
   templateUrl: './single-candidate.component.html',
   styleUrl: './single-candidate.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SingleCandidateComponent {
+export class SingleCandidateComponent implements OnInit{
+ //nous avons besoin d'un loading pour charger le spinner d'attente
+ //et d'un candidat
+  loading$!: Observable<boolean>
+ candidate$!: Observable<Candidate>
+ constructor(private candidatesServices: CandidateService, private route: ActivatedRoute, private router: Router){}
+
+  ngOnInit(): void {
+    this.initObservable()
+
+  }
+  private initObservable():void{
+    /**on initialise les observables */
+    this.loading$ = this.candidatesServices.loading$
+    //on recupèrer le candidat dont l'id est dans la route active
+    //tout d'abord en recupérant dans les paramètres de la route active 
+    //l'id avec +params['id'] et le signe + permet de caster cet id 
+    // en number car il peut etre un string 
+    this.candidate$ = this.route.params.pipe(
+      switchMap(params => this.candidatesServices.getCandidateById(+params['id']))
+  );
+  }
+  onHire():void{}
+  onGoBack():void{ this.router.navigateByUrl('/reactive-state/candidates');}
+  onRefuse():void{}
 
 }
--- src/app/reactive-state/services/candidatas.service.ts
+++ src/app/reactive-state/services/candidatas.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { BehaviorSubject, delay, Observable, tap } from "rxjs";
+import { BehaviorSubject, delay, map, Observable, tap } from "rxjs";
 import { Candidate } from "../models/candidate.model";
 import { environment } from "../../../environments/environment.prod";
 
@@ -33,15 +33,40 @@ export class CandidateService {
          * Observables recevront cette nouvelle donnée.*/
         this._loading$.next(loading);
       }
-
+      //on doit s'amuser à ce que les chargements ne se font 
+      // qu'après 5 minutes les un des autres
+      //donc un chargement ne sera fait que si cà fait au moins 5 secondes qu'on a fait le dernier chargement
+      private lastCandidatesLoad = 0;
       getCandidatesFromServer() {
+        //implementons la logique de chargement rien qu'après au moins 5(5000 milisecondes) secondes du dernier chargement
+        if (Date.now()-this.lastCandidatesLoad<5000){
+            return // pas de chargement si il y a moins de 5 secondes que le dernier chargement s'est effectué
+        }
         this.setLoadingStatus(true);
         this.http.get<Candidate[]>(`${environment.apiUrl}candidates`).pipe(
           delay(5000),
           tap(candidates => {
             this._candidates$.next(candidates);
             this.setLoadingStatus(false);
+            //actualiser la date du dernier chargement
+            this.lastCandidatesLoad = Date.now();
           })
         ).subscribe();
     }
+
+    getCandidateById(id: number): Observable<Candidate> {
+      /***on essaye dans cette méthode de recupérer une lis de candidat ayant 
+       * l'id donnée en paramètre et on prend l'ément du rang 1 bien cette liste ait 
+       * pour l'essentiel un seul élément
+       */
+      //si ce n'est pad  le dernier chargement réalisé, alors 
+      //on réalise un nouveau chargement à partir du serveur
+      if (!this.lastCandidatesLoad) {
+        this.getCandidatesFromServer();
+    }
+      return this.candidates$.pipe(
+          map(candidates => candidates.filter(candidate => candidate.id === id)[0])
+      );
+  }
+
 }
